6687e269989c9b69721f165d7494a8980ff2a610
Robert Dean <robert@flexbase.app>
2023-05-24 08:49:07 -0400

    transform dotenv obj to have types

--- create-env/action.js
+++ create-env/action.js
@@ -6,17 +6,64 @@ const dotenv = require('dotenv');
 const fileName = core.getInput('file_name');
 const filePath = core.getInput('file_path');
 const fileData = core.getInput('file_data');
+const fileDataFormat = core.getInput('file_data_format');
 const fileFormat = core.getInput('file_format');
+
+const isNumber = (value) => {
+  if (typeof value === 'number') {
+    return true;
+  }
+
+  return typeof value === 'string' && !Number.isNaN(value) && !Number.isNaN(Number.parseFloat(value));
+};
+
+const isBoolean = (value) => {
+  if (typeof value === 'boolean') {
+    return true;
+  }
+
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  if (['true', '1', 'yes', 'on', 'false', '0', 'no', 'off'].includes(value.trim().toLowerCase())) return true;
+
+  return false;
+};
+
+const getType = (value) => {
+  if (isNumber(value)) {
+    return 'number';
+  }
+  if (isBoolean(value)) {
+    return 'boolean';
+  }
+
+  return typeof value;
+};
+
+const dotenvParse = (fileData) => {
+  const fileData = dotenv.parse(fileData);
+
+  const obj = {};
+
+  Object.entries(fileData).forEach((entry) => {
+    const entryType = getType(entry[1]);
+    obj[entry[0]] = entryType === 'number' ? Number(entry[1]) : entryType === 'boolean' ? Boolean(entry[1]) : entry[1];
+  });
+
+  return obj;
+};
 
 try {
   const outputFile = path.join(filePath, fileName);
 
   const existingBuffer = fs.existsSync(outputFile) ? fs.readFileSync(outputFile) : Buffer.from('');
 
+  const parsedFileData = fileDataFormat == 'dotenv' ? dotenvParse(fileData) : JSON.parse(fileData);
+
   const existing =
-    fileFormat === 'json'
-      ? { ...JSON.parse(existingBuffer), ...JSON.parse(fileData) }
-      : { ...dotenv.parse(existingBuffer), ...dotenv.parse(fileData) };
+    fileFormat === 'json' ? { ...JSON.parse(existingBuffer), ...parsedFileData } : { ...dotenv.parse(existingBuffer), ...parsedFileData };
 
   const output =
     fileFormat === 'json'
